73b8f68383787f890e053d3874df5ac26c512b05
Focusucof <imthefirstfocus@gmail.com>
2021-10-13 19:58:03 -0400

    heroku port variable

--- src/index.js
+++ src/index.js
@@ -234,7 +234,7 @@ app.post('/', async function(req, res) {
     res.send('OK');
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('Server running on port 3000'));
 
 
 async function getRank(rankID) {
